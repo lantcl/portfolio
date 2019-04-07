a23201e4ef223e34af5b34016b5772f8ac95e26e
lantcl <cleolant@gmail.com>
2019-04-07 14:04:45 -0400

    fun stuff

---
+++ home - Copy.php
@@ -0,0 +1,192 @@
+<?php
+
+//$guest = $_POST['entry'];
+//$guest = "cherry";
+
+$dsn = "mysql:host=localhost;dbname=lantc_portfolio;charset=utf8mb4";
+$dbusername = "lantc";
+$dbpassword = "NkXHus3h!6V";
+
+$pdo = new PDO($dsn, $dbusername, $dbpassword);
+
+$projects = $pdo->prepare("SELECT * FROM `projects`");
+$images = $pdo->prepare("SELECT * FROM `images` WHERE `featured`='1'");
+
+$projects->execute();
+//$backroundimages = $images->execute();
+// $stickerRequest = file_get_contents('https://api.giphy.com/v1/stickers/random?api_key=xBFiRt8PTmOcWKrCeSWgtukA7ZPOy5xa&rating=PG&tag='.$guest);
+// $stickerResult = json_decode($stickerRequest, true);
+// // $sticker = $stickerResult["data"][0]["images"]["fixed_width_small"]["url"];
+
+// $sticker = $stickerResult["data"]["fixed_height_small_url"];
+// //var_dump($sticker);
+?>
+
+<!doctype html>
+<html>
+<!-- <html class="has-navbar-fixed-top">
+ -->    <head>
+        <title>Cleo Lant - Visual Designer | Creative Problem-Solver</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8"/>
+        <!-- <link rel="shortcut icon" href="images/profile_icon.ico" type="image/x-icon" /> -->
+        
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <!-- <link rel="stylesheet" type="text/css" href="css/media-queries.css"> -->
+        
+        <!-- Open Graph Tags -->
+		<!-- for Google -->
+		<meta name="description" content="Cleo is currently a student of the Interactive Media Management program at Sheridan College. She shares her love of storytelling through illustration, motion-graphics, wire-frames, web-development and interactive media. See her portfolio at mynameiscleo.com" />
+		<meta name="keywords" content="Illustration, Visual Designer, UX Design, Creative Problem Solving, Interactive Media, Motion Graphics, Toronto, Sheridan" />
+		<meta name="author" content="Cleo Lant" />
+		<link rel="author" href="https://www.mynameiscleo.com/">
+		<meta name="copyright" content="2019" />
+
+
+		<!-- for Facebook -->
+		<meta property="og:title" content="Cleo Lant - Visual Designer" />
+		<meta property="og:site_name" content="Cleo Lant Resume" />
+		<meta property="og:url" content="http://lantc.dev.fast.sheridanc.on.ca/resume/mobileresume.html" />
+		<meta property="og:type" content="website" />
+		<meta property="og:description" content="Cleo is currently a student of the Interactive Media Management program at Sheridan College. She shares her love of storytelling through illustration, motion-graphics, wire-frames, web-development and interactive media. See her portfolio at mynameiscleo.com" />
+		<meta property="og:image" content="http://lantc.dev.fast.sheridanc.on.ca/resume/images/facebook.jpg" />
+		<meta property="og:image:type" content="image/jpeg" /> 
+		<meta property="og:image:width" content="1200" /> 
+		<meta property="og:image:height" content="630" /> <!-- Facebook recommended: 1200 x 630 - ideal: 1200 x 1200 square image -->
+
+
+		<!-- for Twitter -->
+		<meta name="twitter:card" content="Cleo Lant is a Toronto-based illustrator and visual designer." />
+		<meta name="twitter:title" content="Cleo Lant - Visual Designer" />
+		<meta name="twitter:description" content="Cleo is currently a student of the Interactive Media Management program at Sheridan College. She shares her love of storytelling through illustration, motion-graphics, wire-frames, web-development and interactive media. See her portfolio at mynameiscleo.com" />
+		<meta name="twitter:url" content="http://lantc.dev.fast.sheridanc.on.ca/resume/mobileresume.html" />
+		<meta name="twitter:image" content="http://lantc.dev.fast.sheridanc.on.ca/resume/images/twitter.jpg" />
+
+		<!--  Images for the Twitter Card should be at least 280px in width, and at least 150px in height. Image must be less than 1MB in size. Ideal is 1,024 x 512 -->
+
+	<!--[if IE]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+  	<!--[if lt IE 9]>
+    <script src="http://css3-mediaqueries-js.googlecode.com/files/css3-mediaqueries.js"></script>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+    </head> 
+    <body>
+    
+    	<header>
+    		<nav id="no-bg" class="navbar is-fixed-top is-marginless" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span></a>
+                </div>
+                <div class="navbar-menu">
+                    <div class="navbar-start">
+                        <a href="home.php#page-portfolio" class="navbar-item">Portfolio</a>
+                        <a href="resume.php" class="navbar-item">Resume</a>
+                        <a href="home.php#page-contact" class="navbar-item">Contact</a>
+                        <a href="guestbook.php" class="navbar-item">Guestbook</a>
+                    </div>
+                </div>
+            </nav>
+    	</header>
+    	<main>
+            
+        <a name="page-top"></a>
+        <section class="hero is-dark is-bold is-fullheight" id="page-intro">
+            <div class="hero-body">
+        	   <div class="container">
+        		  <h1 class="white">Hi,</h1>
+        		  <h1 class="white">my</h1>
+        		  <h1 class="white">name is</h1>
+                  <h1 >Cleo</h1>
+                <h4 class="is-italic">I design, paint, draw, animate, build, code, ideate and iterate my way to <span class="yellow">creative solutions</span></h4>                
+            </div> 
+            </div>
+            <div class="level">
+                    <div class="level-item is-marginless"><a href="#page-portfolio"><h4 id="goTo-portfolio" ">Portfolio</h4></a></div>
+                    <div class="level-item is-marginless"><a class="nav-arrow" href="#page-portfolio"><i class="fas fa-chevron-down fa-lg" ></i></a></div>
+                </div>
+            </div>
+        </section>
+        
+        <a name="page-portfolio"></a>
+        <section class="hero is-black is-fullheight"">
+            <div class="hero-body">
+               <div class="container">
+	    		<h2>Portfolio</h2>
+    	    		<?php while($row = $projects->fetch()){ ?>
+                        <h3><?php echo($row["projectName"]) ?></h3>
+    	    			<a role="button" href="project.php?id=<?php echo($row["id"]);?>">
+                            <img src="img/<?php echo($row["projectThumbnail"]);?>" class="image is-128x128">
+                        </a>
+                        <span class="tag is-dark"><?php echo($row["tag1"]);?></span>
+                        <span class="tag is-dark"><?php echo($row["tag2"]);?></span>
+                        <span class="tag is-dark"><?php echo($row["tag3"]);?></span>
+    	    		<?php }?>
+                    <div class="level ">
+                        <div class="level-item is-marginless"><a href="#page-resume"><h4 id="goTo-resume" ">Resume</h4></a></div>
+                        <div class="level-item is-marginless"><a class="nav-arrow" href="#page-resume"><i class="fas fa-chevron-down fa-lg" ></i></a></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <a name="page-resume"></a>
+        <section class="hero is-dark is-fullheight"">
+            <div class="hero-body">
+               <div class="container">
+                    <h2>Resume</h2>
+                    <p>sdgasgsdgsdgsdg</p>
+
+                    <div class="level ">
+                        <div class="level-item is-marginless"><a href="#page-contact"><h4 id="goTo-contact" ">Contact</h4></a></div>
+                        <div class="level-item is-marginless"><a class="nav-arrow" href="#page-contact"><i class="fas fa-chevron-down fa-lg" ></i></a></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <a name="page-contact"></a>
+        <section class="hero is-dark is-bold is-fullheight"">
+            <div class="hero-body">
+               <div class="container">
+        			<h2>Contact</h2>
+        			<h3>About Me</h3>
+        			<img src="assets/cleo-lant.jpg">
+        			<p></p>
+        
+                    <!-- social links -->
+
+        			<a role="button" href="mailto:lantc@sheridancollege.ca"><img src="assets/email.png"></a>
+        			<a role="button" href="tel:289-400-1859"><img src="icons/phone.png"></a>
+        			<a role="button" href="https://www.mynameiscleo.com/">Portfolio</a>
+        			<a role="button" href="https://www.linkedin.com/in/cleolant/">LinkedIn</a>
+
+                    <div class="level">
+                        <div class="level-item is-marginless"><a href="#page-top"><h4 id="goTo-top" ">Top</h4></a></div>
+                        <div class="level-item is-marginless "><a class="nav-arrow" href="#page-top"><i class="fas fa-chevron-up fa-lg" ></i></a></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    		<a role="button"><img src="assets/down.png"></a>
+    		<!-- JS on click even using this and current position -->
+    	</main>
+        </section>
+    	<footer>
+    		
+    	</footer>
+    	<script type="text/javascript" src="js/jquery-3.3.1.js"></script>
+    	<script type="text/javascript" src="js/particles.js"></script>
+    	<script type="text/javascript" src="js/script.js"></script>
+    </body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,117 @@
+// /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+// particlesJS.load('particles-js', 'assets/particles.json', function() {
+//   console.log('callback - particles.js config loaded');
+// });
+particlesJS('particles-js',
+  
+{
+  "particles": {
+    "number": {
+      "value": 53,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#f5faa9"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 3
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 8,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 523.1949773282179,
+        "size": 141.952125631687,
+        "duration": 6.408124528516157,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 48.669300216578414,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+}
+);
